29eb07aff70113786ffacf359a5833ff65b75236
Vito217 <vito.segurac@hotmail.com>
2020-10-23 23:13:40 +0200

    Some files update

--- Library/PackageCache/com.unity.vectorgraphics@2.0.0-preview.12/Runtime/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.VectorGraphics.Tests")]
-[assembly: InternalsVisibleTo("Unity.VectorGraphics.Editor")]
-[assembly: InternalsVisibleTo("Unity.VectorGraphics.Editor.Tests")]
-[assembly: InternalsVisibleTo("Unity.InternalAPIEngineBridge.003")]
