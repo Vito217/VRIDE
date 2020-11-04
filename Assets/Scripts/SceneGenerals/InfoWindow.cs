d5193330d4f6e9ebe05f251f49d73baaf95e8e23
Vito217 <vito.segurac@hotmail.com>
2020-11-03 22:15:58 -0300

    text with tasks

---
+++ Assets/Scripts/SceneGenerals/InfoWindow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoWindow : MonoBehaviour
+{
+    public Transform playerCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(
+            transform.position.x, playerCamera.position.y + 1f, transform.position.z);
+    }
+}
