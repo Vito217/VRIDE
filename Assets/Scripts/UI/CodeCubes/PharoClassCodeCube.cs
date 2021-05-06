5fe8cfb5be56cbd7a5040e1dac7948bccf077d4a
Victor Segura <vito.segurac@hotmail.com>
2021-05-06 16:44:49 -0400

    Added basic animations to codecube

--- Assets/Scripts/UI/CodeCubes/PharoClassCodeCube.cs
+++ Assets/Scripts/UI/CodeCubes/PharoClassCodeCube.cs
@@ -3,15 +3,11 @@ using UnityEngine;
 
 public class PharoClassCodeCube : PharoCodeCube
 {
-    List<CodeCube> childCubes;
-
+    public List<Transform> childLists;
     private bool opened = false;
 
     public override void InnerStart()
     {
-        childCubes = new List<CodeCube>();
-        foreach (Transform child in transform)
-            childCubes.Add(child.gameObject.GetComponent<CodeCube>());
 
         base.InnerStart();
     }
@@ -23,38 +19,86 @@ public class PharoClassCodeCube : PharoCodeCube
             opened = true;
 
             RotateLocally(360);
-            ScaleTo(new Vector3(.4f, .4f, .4f));
+            //ScaleTo(new Vector3(.4f, .4f, .4f));
 
-            childCubes[0].MoveLocallyTo(new Vector3(1f, 0f, 0f));
-            childCubes[0].transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            for(int i = 0; i < childLists[0].childCount; i++)
+            {
+                CodeCube child = childLists[0].GetChild(i).GetComponent<CodeCube>();
+                child.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+                child.MoveLocallyTo(new Vector3(i + 1, 0f, 0f));
+            }
 
-            childCubes[1].MoveLocallyTo(new Vector3(0f, 1f, 0f));
-            childCubes[1].transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            for (int i = 0; i < childLists[1].childCount; i++)
+            {
+                CodeCube child = childLists[1].GetChild(i).GetComponent<CodeCube>();
+                child.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+                child.MoveLocallyTo(new Vector3(0f, i + 1, 0f));
+            }
 
-            childCubes[2].MoveLocallyTo(new Vector3(0f, 0f, 1f));
-            childCubes[2].transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            for (int i = 0; i < childLists[2].childCount; i++)
+            {
+                CodeCube child = childLists[2].GetChild(i).GetComponent<CodeCube>();
+                child.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+                child.MoveLocallyTo(new Vector3(0f, 0f, i + 1));
+            }
 
-            childCubes[3].MoveLocallyTo(new Vector3(-1f, 0f, 0f));
-            childCubes[3].transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            for (int i = 0; i < childLists[3].childCount; i++)
+            {
+                CodeCube child = childLists[3].GetChild(i).GetComponent<CodeCube>();
+                child.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+                child.MoveLocallyTo(new Vector3(-(i + 1), 0f, 0f));
+            }
 
-            childCubes[4].MoveLocallyTo(new Vector3(0f, -1f, 0f));
-            childCubes[4].transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            for (int i = 0; i < childLists[4].childCount; i++)
+            {
+                CodeCube child = childLists[4].GetChild(i).GetComponent<CodeCube>();
+                child.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+                child.MoveLocallyTo(new Vector3(0f, -(i + 1), 0f));
+            }
 
-            childCubes[5].MoveLocallyTo(new Vector3(0f, 0f, -1f));
-            childCubes[5].transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            for (int i = 0; i < childLists[5].childCount; i++)
+            {
+                CodeCube child = childLists[5].GetChild(i).GetComponent<CodeCube>();
+                child.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+                child.MoveLocallyTo(new Vector3(0f, 0f, -(i + 1)));
+            }
         }
         else
         {
             opened = false;
 
             RotateLocally(-360);
-            ScaleTo(new Vector3(.5f, .5f, .5f));
-            childCubes[0].MoveLocallyTo(new Vector3(0f, 0f, 0f));
-            childCubes[1].MoveLocallyTo(new Vector3(0f, 0f, 0f));
-            childCubes[2].MoveLocallyTo(new Vector3(0f, 0f, 0f));
-            childCubes[3].MoveLocallyTo(new Vector3(0f, 0f, 0f));
-            childCubes[4].MoveLocallyTo(new Vector3(0f, 0f, 0f));
-            childCubes[5].MoveLocallyTo(new Vector3(0f, 0f, 0f));
+            //ScaleTo(new Vector3(.5f, .5f, .5f));
+
+            for (int i = 0; i < childLists[0].childCount; i++)
+            {
+                childLists[0].GetChild(i).GetComponent<CodeCube>().MoveLocallyTo(new Vector3(0f, 0f, 0f));
+            }
+
+            for (int i = 0; i < childLists[1].childCount; i++)
+            {
+                childLists[1].GetChild(i).GetComponent<CodeCube>().MoveLocallyTo(new Vector3(0f, 0f, 0f));
+            }
+
+            for (int i = 0; i < childLists[2].childCount; i++)
+            {
+                childLists[2].GetChild(i).GetComponent<CodeCube>().MoveLocallyTo(new Vector3(0f, 0f, 0f));
+            }
+
+            for (int i = 0; i < childLists[3].childCount; i++)
+            {
+                childLists[3].GetChild(i).GetComponent<CodeCube>().MoveLocallyTo(new Vector3(0f, 0f, 0f));
+            }
+
+            for (int i = 0; i < childLists[4].childCount; i++)
+            {
+                childLists[4].GetChild(i).GetComponent<CodeCube>().MoveLocallyTo(new Vector3(0f, 0f, 0f));
+            }
+
+            for (int i = 0; i < childLists[5].childCount; i++)
+            {
+                childLists[5].GetChild(i).GetComponent<CodeCube>().MoveLocallyTo(new Vector3(0f, 0f, 0f));
+            }
         }
     }
 }
