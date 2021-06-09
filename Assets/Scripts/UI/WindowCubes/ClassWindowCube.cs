275d70449cf2481003920dbccde681a6c703b792
Victor Segura <vito.segurac@hotmail.com>
2021-06-09 19:00:03 -0400

    Creating new type of window cube

--- Assets/Scripts/UI/WindowCubes/ClassWindowCube.cs
+++ Assets/Scripts/UI/WindowCubes/ClassWindowCube.cs
@@ -1,9 +1,13 @@
+using PharoModule;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ClassWindowCube : WindowCube
 {
+    [HideInInspector]
+    public string className;
+
     // Start is called before the first frame update
     public void OnPressInspection()
     {
@@ -18,5 +22,22 @@ public class ClassWindowCube : WindowCube
     public void OnPressSubClasses()
     {
 
+    }
+
+    async void GenerateSubClasses()
+    {
+
+    }
+
+    async void GenerateParentClass()
+    {
+        string code = await Pharo.Execute(className + " subclasses .");
+    }
+
+    async void GenerateInspectElements()
+    {
+        string code = await Pharo.Execute(className + " definition .");
+        string im = await Pharo.Execute(className + " methodDict keys asString .");
+        string cm = await Pharo.Execute("(" + className + " class) methodDict keys asString .");
     }
 }
